85f25d72264a40fee81bfd49ba81acd74b4f186e
cty0613 <icty0613@gmail.com>
2024-03-02 16:10:55 +0900

    result text match init

--- js/load.js
+++ js/load.js
@@ -2,7 +2,7 @@ function redirectToNewPage() {
     setTimeout(function() {
         var newUrl = "../pages/result.html";
         window.location.href = newUrl;
-    }, 5000); 
+    }, 2000); 
 }
 
 redirectToNewPage();
